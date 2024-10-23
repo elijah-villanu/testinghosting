c9e1286f176de6fc2d7fbfef9b4d620be4a2a81e
Hunter Lathery <hlathery@calpoly.edu>
2024-10-23 11:22:08 -0700

    Removed some endpoints

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import carts, catalog, bottler, barrels, admin, info, inventory
+from src.api import catalog, admin, info, barrels
 import json
 import logging
 import sys
@@ -24,21 +24,11 @@ app = FastAPI(
 
 origins = ["https://potion-exchange.vercel.app"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "OPTIONS"],
-    allow_headers=["*"],
-)
 
-app.include_router(inventory.router)
-app.include_router(carts.router)
 app.include_router(catalog.router)
-app.include_router(bottler.router)
-app.include_router(barrels.router)
 app.include_router(admin.router)
 app.include_router(info.router)
+app.include_router(barrels.router)
 
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
